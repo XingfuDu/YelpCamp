ce6b6734f6216e8942fddb8bf56c6e016b2bc053
duxingfu <du.xingf@northeastern.edu>
2021-02-27 04:45:55 -0500

    add show page

--- app.js
+++ app.js
@@ -22,9 +22,16 @@ app.set('views',path.join(__dirname,'views'));
 
 app.get('/',(req,res)=>{
     res.render('home');
-})
-
+});
+app.get('/campgrounds',async(req,res)=>{
+    const campgrounds=await Campground.find({});
+    res.render('campgrounds/index',{campgrounds});
+});
+app.get('/campgrounds/:id',async(req,res)=>{
+    const campground=await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground});
+});
 
 app.listen(3000,()=>{
-    console.log('Sering on port 3000')
-})
+    console.log('Sering on port 3000');
+});
