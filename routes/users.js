8b6aa6258ac7a832a1d12ab922d0302cd60b9168
duxingfu <du.xingf@northeastern.edu>
2021-03-05 16:16:42 -0500

    add controllers and star rating

--- routes/users.js
+++ routes/users.js
@@ -1,41 +1,14 @@
 const express=require('express');
 const router=express.Router();
-const User=require('../models/user');
 const catchAsync=require('../utils/catchAsync');
 const passport=require('passport');
+const users=require('../controllers/users');
 
-router.get('/register',(req,res)=>{
-    res.render('users/register');
-});
-router.post('/register',catchAsync(async(req,res)=>{
-    try {
-        const {email,username,password}=req.body;
-        const user=new User({email,username});
-        const registeredUser=await User.register(user,password);
-        req.login(registeredUser,err=>{
-            if(err) {
-                return next(err);
-            }
-            req.flash('success','Welcome to Yelp Camp!');
-            res.redirect(('/campgrounds'));
-        });
-    } catch (e) {
-        req.flash('error',e.message);
-        res.redirect('register');
-    }
-}));
-router.get('/login',(req,res)=>{
-    res.render('users/login');
-});
-router.post('/login',passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),(req,res)=>{
-    req.flash('success','Welcome back!');
-    const redirectUrl=req.session.returnTo || '/campgrounds';
-    res.redirect(redirectUrl);
-});
-router.get('/logout',(req,res)=>{
-    req.logout();
-    req.flash('success','goodbye')
-    res.redirect('/campgrounds');
-});
+router.route('/register').get(users.renderRegister)
+      .post(catchAsync(users.register));
+router.route('/login')
+    .get(users.renderLogin)
+    .get(users.logout);
+router.post('/login',passport.authenticate('local',{failureFlash:true,failureRedirect:'/login'}),users.login);
 
 module.exports=router;
